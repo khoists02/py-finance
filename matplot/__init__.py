e8661afd54c870f8eb9f06c03c02d39400481fe6
KhoiLe <khoi.kioto@gmail.com>
2024-08-17 18:17:20 +0700

    refactored

--- matplot/__init__.py
+++ matplot/__init__.py
@@ -1,25 +1,30 @@
 import matplotlib.pyplot as plt
 
+FONTDICS = {'fontname': 'Comic Sans MS', 'fontsize': 16, 'color': '#d1d1d1', 'fontweight': 'bold'}
+
 
 class Matplot:
-    instance = None
-    FONTDICS = {'fontname': 'Comic Sans MS', 'fontsize': 16, 'color': '#d1d1d1', 'fontweight': 'bold'}
 
-    def __init__(self, x, y, plots):
+    def __init__(self, x, y, plots, fontdict=None):
+        if fontdict is None:
+            fontdict = FONTDICS
+        else:
+            self.fontdict = fontdict
         self.x = x
         self.y = y
         self.plots = plots
 
     def draw(self):
         types = ['b.-', 'r.-', 'g.-']  # type plot chart
-        plt.title('TESTING', fontdict=self.FONTDICS)  # title and font config
+        plt.title('TESTING', fontdict=self.fontdict)  # title and font config
         for idx, dt in enumerate(self.plots):
+            # TODO: multple plot
             plt.plot(self.x, self.y[idx], types[idx], label=dt)  # plot data
 
-        # TODO: how to shoe legends.
+        # TODO: how to show legends.
         plt.xlabel('Time')
         plt.ylabel('Dolar')
-        # show label with label in plot
+        # TODO show label with label in plot
         plt.legend()
-        # show chart
+        # TODO show chart
         plt.show()
