56dbb6bf6469f6b1e9ae8b37855f9924b6bdd2a6
KhoiLe <khoi.le@advahealthsolutions.com>
2024-08-17 16:05:03 +0700

    add multiple

--- data/info.py
+++ data/info.py
@@ -16,7 +16,63 @@ class FinancialData:
     def get_quarterly_cashflow_keys(self) -> list[str]:
         return  list(self.get_quarterly_cashflow().keys())
 
-    def get_quarterly_cashflow_keys_of_values(self) -> list[str]:
+    ### TODO: labels cashflow
+    """
+        Free Cash Flow
+        Repurchase Of Capital Stock
+        Issuance Of Capital Stock
+        Capital Expenditure
+        Interest Paid Supplemental Data
+        End Cash Position
+        Beginning Cash Position
+        Changes In Cash
+        Financing Cash Flow
+        Cash Flow From Continuing Financing Activities
+        Net Other Financing Charges
+        Proceeds From Stock Option Exercised
+        Net Common Stock Issuance
+        Common Stock Payments
+        Common Stock Issuance
+        Investing Cash Flow
+        Cash Flow From Continuing Investing Activities
+        Net Other Investing Changes
+        Net Investment Purchase And Sale
+        Sale Of Investment
+        Purchase Of Investment
+        Net Business Purchase And Sale
+        Sale Of Business
+        Purchase Of Business
+        Net PPE Purchase And Sale
+        Purchase Of PPE
+        Operating Cash Flow
+        Cash Flow From Continuing Operating Activities
+        Change In Working Capital
+        Change In Other Current Liabilities
+        Change In Other Current Assets
+        Change In Payables And Accrued Expense
+        Change In Accrued Expense
+        Change In Payable
+        Change In Account Payable
+        Change In Prepaid Assets
+        Change In Inventory
+        Change In Receivables
+        Changes In Account Receivables
+        Other Non Cash Items
+        Stock Based Compensation
+        Provision and Write Off of Assets
+        Asset Impairment Charge
+        Amortization Of Securities
+        Deferred Tax
+        Deferred Income Tax
+        Depreciation Amortization Depletion
+        Depreciation And Amortization
+        Depreciation
+        Operating Gains Losses
+        Gain Loss On Investment Securities
+        Gain Loss On Sale Of Business
+        Net Income From Continuing Operations
+    """
+    def get_labels_of_cashflow_obj(self) -> list[str]:
         allvalues = list(self.get_quarterly_cashflow().values())
         one_vl = allvalues[0]
         return  list(one_vl.keys())
--- main.py
+++ main.py
@@ -5,21 +5,20 @@ from helpers.format import format_date_from_timestamp
 from helpers.helper import reverse
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-FONTDICS = {'fontname': 'Comic Sans MS', 'fontsize': 16, 'color': '#d1d1d1'}
-
-# Multiple data plot
-
 plots = ['Financing Cash Flow', 'Free Cash Flow', 'Sale Of Investment']
-
 # default_data = np.array([[1,2], [3,4], [5,6]])
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # TODO: new instance
+    ### TODO: load data
     new_data = FinancialData("LAZR")
     keys = new_data.get_quarterly_cashflow_keys()
+    # defined init x, y values
     k_labels = []
     k_values = []
+
+    print(new_data.get_labels_of_cashflow_obj())
     for i, plt in enumerate(plots):
         # TODO: Convert keys to datetime format.
         values = []
@@ -34,6 +33,6 @@ if __name__ == '__main__':
         k_values.append(result_arr)
 
         k_labels = labels
-    # TODO: show chart.
-    plot = Matplot(reverse(k_labels), k_values, plots)
-    plot.draw()
+    # TODO: Matplot work.
+    # plot = Matplot(reverse(k_labels), k_values, plots)
+    # plot.draw()
